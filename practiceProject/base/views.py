7691fa73256af3a92e0131796261c4f18346db61
Subin <subinsedhai002@gmail.com>
2022-08-20 16:10:07 +0545

    [Adding data to show in home page]

--- practiceProject/base/views.py
+++ practiceProject/base/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 # Create your views here.
 
+products = [
+    {'id' : 1, 'name' : 'Airsoft Gun'},
+    {'id' : 2, 'name' : 'Scope'},
+    {'id' : 3, 'name' : 'Mag safe'},
+    {'id' : 4, 'name' : 'Helmet'},
+]
+
 def home(request):
-    return render(request, 'home.html')
+    # I am sending above products data as dictonary which will be called 'products'
+    return render(request, 'home.html', {'products' : products})
 
 def product(request):
     return render(request, 'product.html')
