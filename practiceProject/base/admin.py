ea306029c650f9c3a263803c2770ea0c8fb6b132
Subin <subinsedhai002@gmail.com>
2022-09-01 11:13:26 +0545

    [Ops! forget to add database to admin file]

--- practiceProject/base/admin.py
+++ practiceProject/base/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Products
+from .models import Products, Review
 
 #add into database in app
 
 admin.site.register(Products)
+admin.site.register(Review)
