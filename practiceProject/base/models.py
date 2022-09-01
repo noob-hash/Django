4c0e037e73bde0cfa281c5be3342c2f077cb0ac8
Subin <subinsedhai002@gmail.com>
2022-09-01 11:04:58 +0545

    [Added review, user and other changes]

--- practiceProject/base/models.py
+++ practiceProject/base/models.py
@@ -1,4 +1,6 @@
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,13 +8,15 @@ from django.db import models
 #instance's first word is capatilized best practice
 
 class Products(models.Model):
+    #user needs to be added
+    user = models.ForeignKey(User, on_delete = models.CASCADE, null = True)
     #columns and it's data type ant other values for database
     name = models.CharField(max_length=200)
 
     #first for database, second for form to let empty value allowed
     productDescription = models.TextField(null=True, blank=True)
-    firstPlaced = models.DateTimeField(auto_now_add=True)
-    lastUpdated = models.DateTimeField(auto_now=True)
+    createdDate = models.DateTimeField(auto_now_add=True)
+    updatedDate = models.DateTimeField(auto_now=True)
 
     #diff between auto_now and auto_now_add is
     #auto_now updates every time this instance is used
@@ -21,3 +25,15 @@ class Products(models.Model):
     #return when called aka methods
     def __str__(self):
         return self.name
+
+class Review(models.Model):
+    #user needs to be added
+    user = models.ForeignKey(User, on_delete = models.CASCADE, null = True)
+    product = models.ForeignKey(Products, on_delete = models.CASCADE, null = True)
+    star = models.IntegerField(default = 0)
+    comment = models.TextField(null = True, blank = True)
+    createdDate = models.DateTimeField(auto_now_add=True)
+    updatedDate = models.DateTimeField(auto_now=True)
+
+    def _str_(self):
+        return {'star' : self.star, 'comment': self.comment[0:50]}
